08a9f765767d893c78dce1cbd6b68046fd339504
Datdianguc <tiendat989@gmail.com>
2022-10-19 22:16:50 +0700

    up Buoi7 up Buoi8

---
+++ Buoi7/index.js
@@ -0,0 +1,19 @@
+'use strict'
+
+let a = 150;
+let b = 10;
+let Name = "Nguyen Tien Dat";
+let tong = a+b;
+let hieu = a-b;
+let tich = a*b;
+let thuong = a/b;
+
+if (0<a<99.9) {
+    console.log('a<100');
+} if (100<a<199.9) {
+    console.log('100<a<200');
+} if (200<a<299.9) {
+    console.log('200<a<300');
+} else {
+    console.log(a>300);
+}
---
+++ Buoi8/index.js
@@ -0,0 +1,17 @@
+
+let x = prompt("Input x: ");
+
+if (x % 2 ==0){
+    console.log('Số chẵn');
+} else if (x % 3 ==0){
+    console.log('Chia hết cho 3');
+} else if (x % 5 == 0){
+    console.log('Chia hết cho 5');
+} else if (x % 3 ==1){
+    console.log('Chia 3 dư 1');
+} else if (x % 3 ==2){
+    console.log('Chia 3 dư 2');
+} else if (x % 3 ==3){
+    console.log('Chia 3 dư 3');
+}
+ 
