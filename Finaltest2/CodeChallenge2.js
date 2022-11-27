95e6835f2eb5d07956d020daf110a6ab480c52e9
Datdianguc <tiendat989@gmail.com>
2022-11-28 02:20:15 +0700

    Last alterations

---
+++ Finaltest2/CodeChallenge2.js
@@ -0,0 +1,18 @@
+const result = Math.floor(Math.random() * 10) + 1;
+let lives = 3;
+function GuessLuckyNumber() {
+    const x = document.getElementById("Number").value;
+    if (x > 10 || x < 1) {
+        console.log(" Số " + x + " không nằm trong khoảng cho phép, mời nhập lại ");
+    } else if (x != result) {
+        console.log("Đoán lại đi");
+        lives -= 1;
+        console.log("Số lượt đoán còn lại = " + lives);
+        if (lives == 0){
+        console.log("Bạn thua");
+    }
+    } else {
+        console.log("Chúc mừng bạn đã trúng thưởng");
+    }
+}
+
